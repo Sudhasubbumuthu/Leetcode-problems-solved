1a85638565a85b30bcb47550ecd365f4de240bd2
Sudhasubbumuthu <115452718+Sudhasubbumuthu@users.noreply.github.com>
2024-02-27 06:00:35 +0530

    Time: 2 ms (97.09%), Space: 41.7 MB (98.14%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
         List<String> ans = new ArrayList<>();
-        String str = String.join("", words);
+        String str = String.join(" ", words);
         for (int i = 0; i < words.length; i++) {
             if (str.indexOf(words[i]) != str.lastIndexOf(words[i]))
                 ans.add(words[i]);
